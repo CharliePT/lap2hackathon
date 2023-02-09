fcca55641fcda766f6813ae4e1643a4b1e43a65b
Charlie <c.thurlow.ct@googlemail.com>
2023-02-09 15:21:39 +0000

    added MVC folders

---
+++ server/routes/post.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const postController = require('../controllers/post.js');
+const postRouter = Router();
+postRouter.get("/", authenticator, postController.index);
+postRouter.post("/", postController.create);
+postRouter.get("/:id", postController.show);
+postRouter.delete("/:id", postController.destroy);
+module.exports = postRouter;
